7c1c38066e0629b7b051e8cca9f9813f85188614
Mike Godenzi <m.godenzi@papers.ch>
2024-04-15 15:59:59 +0200

    fix/deployment

--- src/environments/environment.cat-test.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  backendUrl:
-    'https://cat-test-multisig-orchestration-backend.prod.gke.papers.tech',
-}
---
+++ src/environments/environment.tzbtc-dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  backendUrl: 'https://tzbtc-foundry-backend.dev.gke.papers.tech',
+}
